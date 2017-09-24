f793c42a9d2e81f337dca46186799dfeb27c9edf
[01;31m[K"name"[m[K: "Marissa Tomko <[01;31m[K"email"[m[K: null>
2017-09-24 15:40:11 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :concerts, only: [:index, :new, :show]
+  resources :attendances, only: [:index, :new, :create]
+  resources :users, only: [:new, :create, :index, :delete]
+
+  root 'concerts#index'
 end
